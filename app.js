0f2b45460181f2f0c142d4365375c133fe99f2b5
David Bin Neun <davidbinneun@gmail.com>
2021-03-03 13:15:30 +0200

    added pug render engine

--- app.js
+++ app.js
@@ -5,34 +5,34 @@ const app = express();
 const dir = process.env.NODE_ENV === 'test' ? './test':'./b';
 const DataBase = require('./database');
 
+app.set('view engine', 'pug');
+
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 
 app.use("/public", express.static(`./public`));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  // res.sendFile(__dirname + "/views/index.html");
+  res.render('index');
 });
 
 app.post("/api/shorturl/new", async (req, res) => {
-  await DataBase.addURL(req.body);
-  res.send("success");
+  let newID = await DataBase.addURL(req.body);
+  // res.sendFile(__dirname + "/views/new.html");
+  res.render('new', { title: 'Hey', id: "http://" + req.get('host') + "/" + newID});
 });
 
 app.get("/:id", async (req, res) => {
   let redirectUrl = await DataBase.getOriginalUrl(req.params.id);
-  if (redirectUrl == null)
-    res.sendStatus(404);
-  else
-    res.redirect(redirectUrl);
+  if (redirectUrl == null) res.sendStatus(404);
+  else res.redirect(redirectUrl);
 });
 
 app.get("/api/statistic/:id", async (req, res) => {
   let item = await DataBase.getItem(req.params.id);
-  if (item == null)
-    res.sendStatus(404);
-  else
-    res.json(item);
+  if (item == null) res.sendStatus(404);
+  else res.render('statistic', { title: 'Hey', creationDate: item.creationDate, originalUrl: item.originalUrl, redirectCount: item.redirectCount, id: item.id });
 });
 
 module.exports = app;
--- database.js
+++ database.js
@@ -4,6 +4,7 @@ const fs = require('fs').promises;
 class DataBase {
     static items = [];
 
+    // Gets all data from the JSON file into the items array
     static async readAllData(){
         const data = await fs.readFile('./data.json', 'utf8' , (err, data) => {
             if (err) {
@@ -11,16 +12,30 @@ class DataBase {
               return;
             }
           });
+
           let dataParsed = JSON.parse(data);
           this.items = dataParsed.record;
     }
 
+    // Receives URL, adds it to database and returns the id given to it
     static async addURL(body){
         await this.readAllData();
-        this.items.push({creationDate: Date.now(), redirectCount: 0, originalUrl: body.url, id: shortid.generate()});
+        
+        // Check if URL exists in database
+        for(let item of this.items){
+            if (body.url == item.originalUrl){
+                return item.id; // URL exists, returns its id
+            }
+        }
+
+        // If URL is new, add to database and return id
+        let newItem = {creationDate: Date.now(), redirectCount: 0, originalUrl: body.url, id: shortid.generate()};
+        this.items.push(newItem);
         fs.writeFile(`data.json`, JSON.stringify({"record": this.items}));
+        return newItem.id;
     }
 
+    // Receives id, returns the URL it has
     static async getOriginalUrl(id){
         await this.readAllData();
         for (let item of this.items){
@@ -34,6 +49,7 @@ class DataBase {
         return null;
     }
 
+    // Receives id, returns the full item object
     static async getItem(id){
         await this.readAllData();
         for (let item of this.items){
